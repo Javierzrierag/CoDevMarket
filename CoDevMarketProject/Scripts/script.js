36aaa20d907bd2fa2cd3fdd485dd0b4ed0265378
Javierzrierag <javierzrierag@gmail.com>
2023-06-30 02:28:15 -0300

    Changes: Search and Login

---
+++ CoDevMarketProject/Scripts/script.js
@@ -0,0 +1,5 @@
+document.getElementById("search-button").addEventListener("click", function() {
+  var searchQuery = document.getElementById("search-input").value;
+  // Realizar alguna acción con la consulta de búsqueda (por ejemplo, redireccionar a una página de resultados)
+  console.log("Realizando búsqueda: " + searchQuery);
+});
